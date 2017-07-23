303caf02003d3e41a32b7764e5671455ecb01d7b
Matthew Brown <mattd429@gmail.com>
2017-07-23 19:14:34 -0400

    Update CalculateHelper.java
    Create static final char variables , create method called process.  will create string array -

--- CalculateHelper.java
+++ CalculateHelper.java
@@ -1,1 +1,28 @@
+package com.pluralsight.calengine;
 
+/**
+  * Added to github on 7/23/17
+  */
+public class CalculateHelper {
+    private static final char ADD_SYMBOL = '+';
+    private static final char SUBTRACT_SYMBOL = '-';
+    private static final char MULTIPLY_SYMBOL = '*';
+    private static final char DIVIDE_SYMBOL = '/';
+    MathCommand command;
+    double leftValue;
+    double rightValue;
+    double result;
+  
+  
+  public void process(String statement) {
+    //want to receive a string ass 1.0 2.0
+    //we recieve the complete string.
+    //we then break into individual parts
+    String[] parts = statement.split(""); //this will give you a space
+    String commandString = parts[0];//expect to find the value like add
+    leftValue = Double.parseDouble(part[1]);//convert string ot a double 1.0 as a double
+    rightValue = Double.parseDouble(part[2]);//same as above
+    //We then translate the string command into our math command enumeration
+  
+  }
+}
