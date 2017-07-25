d56a85fc96b50eeb38b5756ac5f47cc7ddd6f36e
Matthew Brown <mattd429@gmail.com>
2017-07-25 13:50:44 -0400

    Update CalculateHelper.java

--- CalculateHelper.java
+++ CalculateHelper.java
@@ -18,11 +18,17 @@ public class CalculateHelper {
     //want to receive a string ass 1.0 2.0
     //we recieve the complete string.
     //we then break into individual parts
-    String[] parts = statement.split(""); //this will give you a space
+    String[] parts = statement.split(" "); //this will give you a space
     String commandString = parts[0];//expect to find the value like add
     leftValue = Double.parseDouble(part[1]);//convert string ot a double 1.0 as a double
     rightValue = Double.parseDouble(part[2]);//same as above
     //We then translate the string command into our math command enumeration
   
   }
+ 
+ private void setCommandStringFromString(String commandString) {
+     //take that string command(add) --> convert into MathCommand.Add
+  
+     
+ }
 }
