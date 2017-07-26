c709e83e459000845fbe8cbabe4d23d1e3d58153
Matthew Brown <mattd429@gmail.com>
2017-07-26 16:37:17 -0400

    Update CalculateBase.java
    use this class as an abstract class, will use the calculate(); in our CalculateHelper.java Class

--- CalculateBase.java
+++ CalculateBase.java
@@ -1,1 +1,36 @@
+package com.pluralsight.calengine;
+/**
+  *Added to Github 7/26/17
+  */
+//we need to declare as abstract since method calculate is going to be intiated in other subclasses.
+//abstract method calculate() will not work without calling the entire class abstract
+public abstract class CalculateBase {
+    private double leftVal;
+    private double rightVal;
+    private double result;
+  
+    public double getLeftVal() { return leftVal; }
+  
+    // saying set method leftval to data type variable leftVal
+    public void setLeftVal(double leftVal) { this.leftVal = leftVal; }
+  
+    public double getRightVal() { return rightVal; }
+  
+    public void setRightVal(double rightVal) { this.rightVal = rightVal; }
+  
+    public double getResult() { return results; }
+  
+    public void setResults(double results) { this.result = results; }
+  
+    public CalculateBase() { }
+    
+    public CalculateBase(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+      
+    }
+    
+    //will use in our CalculateHelper();
+    public abstract void calculate();
 
+}
