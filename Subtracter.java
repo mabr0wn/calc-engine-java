5135ded0431305a23be9080d5c756ce2107bb9ea
Matthew Brown <mattd429@gmail.com>
2017-07-26 16:46:34 -0400

    Update Subtracter.java

--- Subtracter.java
+++ Subtracter.java
@@ -1,1 +1,18 @@
+package com.pluralsight.calengine;
 
+/**
+  *added to github on 07/26/17
+  */
+public class Subtracter extends CalculateBase {
+    
+    public Subtracter() { }
+  
+    public Subtracter(double leftVal, double rightVal) { super(leftVal, rightVal); }
+  
+    @Override
+    public void calculate() {
+      double value = getLeftVal() - getRightVal();
+      setResults(value);
+    }
+
+}
