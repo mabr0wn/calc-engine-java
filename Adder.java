be399601bdd8eb6dbb154588919bf938113fe3ff
Matthew Brown <mattd429@gmail.com>
2017-07-26 16:42:27 -0400

    Update Adder.java

--- Adder.java
+++ Adder.java
@@ -1,1 +1,21 @@
+package com.pluralsight.calengine;
 
+/**
+  * Added to gituhub on 07/26/17
+  */
+//will use this class to set the Add operations.
+// 1.0 + 2.0 = 3.0
+public class Adder extends CalculateBase {
+    
+   public Adder() { }
+  
+   public Adder(double leftVal, double rightVal) { super(leftVal, rightVal); }
+  
+   @Override
+   public void calculate() {
+      double value = getLeftVal() + getRightVal();
+      setResult(value);
+   
+   }
+
+}
