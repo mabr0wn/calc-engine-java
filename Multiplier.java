2bcf69f34dc740f33c24b3400446617e647e3c48
Matthew Brown <mattd429@gmail.com>
2017-07-26 16:55:11 -0400

    Update Multiplier.java

--- Multiplier.java
+++ Multiplier.java
@@ -1,1 +1,17 @@
+package com.pluralsight.calengine;
 
+/**
+  *added to github on 07/26/2017
+  */
+public class Multiplier extends CalculateBase {
+  
+  public Multiplier() { }
+  
+  public Multiplier(double leftVal, double rightVal) { super(leftVal, rightVal); }
+  
+  @Override
+  public void calculate() {
+      double value = getLeftVal() * getRightVal();
+      setResult(value);
+  }
+}
