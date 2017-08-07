dba9f91c3c373e48b83eafc29a3f28c3df708bfb
Matthew Brown <mattd429@gmail.com>
2017-08-07 16:51:06 -0400

    implements MathProcessing
    added the interface MathProcessing

--- Adder.java
+++ Adder.java
@@ -5,17 +5,38 @@ package com.pluralsight.calengine;
   */
 //will use this class to set the Add operations.
 // 1.0 + 2.0 = 3.0
-public class Adder extends CalculateBase {
+public class Adder extends CalculateBase implements MathProcessing {
     
    public Adder() { }
   
    public Adder(double leftVal, double rightVal) { super(leftVal, rightVal); }
-  
+   
+   //will add left and rightVal together
    @Override
    public void calculate() {
       double value = getLeftVal() + getRightVal();
       setResult(value);
    
    }
-
+ 
+   @Override
+   //the keyword is add
+   public String getKeyword() {
+   return "add";
+   }
+   @Override
+   //the symbol is +
+   public char getSymbol() {
+      return '+';
+   }
+   @Override
+   //the doCalculation will add the appropiate values together and get the result using the calculate()
+   public double doCalculation(double leftVal, double rightVal) {
+      setLeftVal(leftVal);
+      setRightVal(rightVal);
+      calculate();
+    
+      return getResult();
+   }
+  
 }
