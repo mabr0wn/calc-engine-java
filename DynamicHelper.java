b4a1cfbd209e34f627a867a59bc2b0e427adc6e1
Matthew Brown <mattd429@gmail.com>
2017-08-07 17:54:25 -0400

    update added MathProcessing[]
    added the array created public method called process running String array called parts[] to split the String Statements [] by space, create theHandler process through the MathProcesses

--- DynamicHelper.java
+++ DynamicHelper.java
@@ -15,4 +15,17 @@ public class DynamicHelper {
   public String process(String statement)
   //what is be process in will be add 1.0 2.0
   //what will be processed out 1.0 + 2.0 = 3.0
+   
+  //split the statement up using the string split method
+  //use the constant that was defined on our MathProcessing called Separator
+  //that constant is a space anytime it see a space it will split the code.
+  String[] parts = statement.split(MathProcessing.SEPARATOR);
+  String keyword = parts[0];//first line in the String array statements which will be add
+  
+  MathProcessing theHandler = null;
+ 
+  //next add a for loop will go through the handlers
+  for (MathProcessing handler : handlers) {
+   //we want to take the current handler we are looping through and call it getKeyword
+  }
 }  
