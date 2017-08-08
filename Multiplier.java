8eae1ad3c3e001fbf2a673d629eafb9c41328d14
Matthew Brown <mattd429@gmail.com>
2017-08-08 10:37:38 -0400

    implements MathProcessing
    added interface MathProcessing - added the @Override method, getKeyword, getSymbol and doCalculation

--- Multiplier.java
+++ Multiplier.java
@@ -3,7 +3,7 @@ package com.pluralsight.calengine;
 /**
   *added to github on 07/26/2017
   */
-public class Multiplier extends CalculateBase {
+public class Multiplier extends CalculateBase implements MathProcessing{
   
   public Multiplier() { }
   
@@ -13,5 +13,23 @@ public class Multiplier extends CalculateBase {
   public void calculate() {
       double value = getLeftVal() * getRightVal();
       setResult(value);
+  }
+ 
+  @Override
+  //the keyword is Multiply
+  public String getKeyword() { return "multiply"; }
+ 
+  @Override
+  //the Symbol is *
+  public char getSymbol() { return '*'; }
+ 
+  @Override
+  //the doCalculation will mulitply the appropiate values together and get the result.
+  public double doCalculation(double leftVal, double rightVal) {
+   setLeftVal(leftVal);
+   setRightVal(rightVal);
+   calculate();
+   
+   return getResult();
   }
 }
