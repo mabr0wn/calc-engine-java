bd30aa6346231aa840669287aaac81abb4c3ebb7
Matthew Brown <mattd429@gmail.com>
2017-08-08 10:48:09 -0400

    implements MathProcessing
    getKeyword, getSymbol

--- Divider.java
+++ Divider.java
@@ -2,8 +2,9 @@ package com.pluralsight.calengine;
 
 /**
   *added to github on 07/26/17
+  *Updated 8/8/17
   */
-public class Divider extends CalculateBase {
+public class Divider extends CalculateBase implements MathProcessing {
   
   public Divider() { }
    
@@ -16,4 +17,20 @@ public class Divider extends CalculateBase {
       double value = getLeftVal() / getRightVal();
       setResult(value);
     }
+ 
+  @Override
+  public String getKeyword() { return "divide"; }
+ 
+  @Override
+  public char getSymbol() { return '/'; }
+ 
+  @Override
+  public double doCalculation(double leftVal, double rightVal) {
+   setLeftVal(leftVal);
+   setRightVal(rightVal);
+   calculate();
+   
+   return getResult();
+   
+  }
 }
