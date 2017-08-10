a46168d8fab538ba1014a0120cfacb9e36c9a1b3
Matthew Brown <mattd429@gmail.com>
2017-08-09 22:22:45 -0400

    Update InvalidStatementException.java

--- InvalidStatementException.java
+++ InvalidStatementException.java
@@ -3,12 +3,15 @@ package com.pluralsight.calengine;
 /**
  *Added to github 8/3
  */
+//Constructor for reason statement is invalid
+//This constructor accepts the information we need.
+//Run this constructor to for first
 public class InvlaidStatementException(String reason, String statement) {
   super(reason + ": " + statement);
   
   
 }
-
+//this constructor allows us to associate another exception with it.(Throwable)
 public class InvalidStatementException(String reason, String statement, Throwable cause) {
   super(reason + ": " + statemnt, cause);
  }
